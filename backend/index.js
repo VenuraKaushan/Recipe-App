78e9544a121c6750a33ccbe72ddfe1a7cb5f77df
Venura Kaushan <86141130+VenuraKaushan@users.noreply.github.com>
2024-10-23 21:14:00 +0530

    asfasf

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,6 @@ const app = express();
 
 const PORT = process.env.PORT || 6001;
 
-//CORS [allows the Vercel requests]
-// app.use(cors({origin: ['https://recipe-app-frontend-hazel.vercel.app'],credentials: true, 
-// }));
-
 
 // CORS [allow the pass the cookies to orin localhost]
 app.use(cors({credentials : true,origin : 'https://recipe-app-frontend-hazel.vercel.app'}));
